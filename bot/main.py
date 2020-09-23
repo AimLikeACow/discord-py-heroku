70fa4f8bd6314ff373b4ed82ed218f8884cb5a49
AimLikeACow <46222526+AimLikeACow@users.noreply.github.com>
2020-09-23 19:25:27 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -21,7 +21,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('ping'):
+        await message.channel.send('pong')
 
 client.run(TOKEN)
