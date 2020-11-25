85bca6e203ed134da8d419eb9e68e57645ede27a
AimLikeACow <46222526+AimLikeACow@users.noreply.github.com>
2020-11-25 19:41:48 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import discord
 
 bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("NzQ3ODkzMjM3MjUwNzg1MzEx.X0Vf2g.XECv1SwDBjgDWRZETAweK_hx4QQ")
 client = discord.Client()
 
 @client.event
